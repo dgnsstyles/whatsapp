df50bc62ce277d0e81b662c0875e53ab1c407671
davidgoren <dgoren@hotmail.com>
2018-02-23 20:33:00 -0300

    first commit

---
+++ whatsapp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  whatsapp
+//
+//  Created by David Goren on 23-02-18.
+//  Copyright © 2018 David Goren. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
